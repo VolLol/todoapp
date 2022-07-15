26c39d5cab2671d77d18f2edf73b5786e8e997f3
VolLol <nurs.ypurs@gmail.com>
2022-07-15 22:28:51 +0300

    Added base table to db. Added entities and their repositories.

--- src/main/resources/db.changelog/scripts/010_added_unique_index_to_users_table.sql
+++ src/main/resources/db.changelog/scripts/010_added_unique_index_to_users_table.sql
@@ -1,5 +1,8 @@
 --liquibase formatted sql
 --changeset Alena:create task comments table
 
-CREATE UNIQUE INDEX users_username_email_key
-    ON users (username, email);
+CREATE UNIQUE INDEX users_username_key
+    ON users (username);
+
+CREATE UNIQUE INDEX users_email_key
+    ON users (email);
