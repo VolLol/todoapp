f686e7bcca4d5eabbd3f472cfd78422290b817d0
VolLol <nurs.ypurs@gmail.com>
2022-08-25 12:18:49 +0300

    Moved swagger annotation from UserHttpController to IUserHttpController

--- src/main/java/ru/alena/todoapp/todoapp/executer/entrypoints/http/controllers/IUserHttpController.java
+++ src/main/java/ru/alena/todoapp/todoapp/executer/entrypoints/http/controllers/IUserHttpController.java
@@ -1,7 +1,8 @@
 package ru.alena.todoapp.todoapp.executer.entrypoints.http.controllers;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.*;
+import org.springframework.web.bind.annotation.*;
 import ru.alena.todoapp.todoapp.executer.entrypoints.http.requests.CreateUserHttpRequest;
 import ru.alena.todoapp.todoapp.executer.entrypoints.http.requests.EditUserHttpRequest;
 import ru.alena.todoapp.todoapp.executer.entrypoints.http.responce.UserCommonResponse;
@@ -11,11 +12,32 @@ import ru.alena.todoapp.todoapp.executer.usecase.usermanage.exceptions.UserNotFo
 
 public interface IUserHttpController {
 
+    @Operation(summary = "Create new user", tags = {"User controller"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "The user was created"),
+            @ApiResponse(responseCode = "400", description = "The user used invalid data")
+    })
     UserCommonResponse userCreate(@RequestBody CreateUserHttpRequest user) throws InvalidUserDateException;
 
+    @Operation(summary = "Edit user", tags = {"User controller"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User data has been changed"),
+            @ApiResponse(responseCode = "400", description = "The user used invalid data"),
+            @ApiResponse(responseCode = "404", description = "The user with the given id was not found")
+    })
     UserCommonResponse userEdit(@RequestBody EditUserHttpRequest request) throws InvalidUserDateException, UserNotFoundException;
 
+    @Operation(summary = "Remove user by id", tags = {"User controller"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The user has been removed"),
+            @ApiResponse(responseCode = "400", description = "The user used invalid data"),
+            @ApiResponse(responseCode = "404", description = "The user with the given id was not found")
+    })
     UserCommonResponse userRemove(@RequestParam String userUUID) throws InvalidUserDateException, UserNotFoundException;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All users have been displayed")})
+    @Operation(summary = "Search all users", tags = {"User controller"})
+    @GetMapping("user/search")
     UserSearchResponse showAllUsers();
 }
--- src/main/java/ru/alena/todoapp/todoapp/executer/entrypoints/http/controllers/UserHttpController.java
+++ src/main/java/ru/alena/todoapp/todoapp/executer/entrypoints/http/controllers/UserHttpController.java
@@ -1,7 +1,5 @@
 package ru.alena.todoapp.todoapp.executer.entrypoints.http.controllers;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.*;
 import org.springframework.web.bind.annotation.*;
 import ru.alena.todoapp.todoapp.executer.entrypoints.http.requests.*;
 import ru.alena.todoapp.todoapp.executer.entrypoints.http.responce.*;
@@ -25,42 +23,21 @@ public class UserHttpController implements IUserHttpController {
         this.userShowAllUsecase = userShowAllUsecase;
     }
 
-    @Operation(summary = "Create new user", tags = {"User controller"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "The user was created"),
-            @ApiResponse(responseCode = "400", description = "The user used invalid data")
-    })
     @PostMapping("user/new")
     public UserCommonResponse userCreate(@RequestBody CreateUserHttpRequest user) throws InvalidUserDateException {
         return userCreateUsecase.execute(user);
     }
 
-    @Operation(summary = "Edit user", tags = {"User controller"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "User data has been changed"),
-            @ApiResponse(responseCode = "400", description = "The user used invalid data"),
-            @ApiResponse(responseCode = "404", description = "The user with the given id was not found")
-    })
     @PutMapping("user/edit")
     public UserCommonResponse userEdit(@RequestBody EditUserHttpRequest request) throws InvalidUserDateException, UserNotFoundException {
         return userEditUsecase.execute(request);
     }
 
-    @Operation(summary = "Remove user by id", tags = {"User controller"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "The user has been removed"),
-            @ApiResponse(responseCode = "400", description = "The user used invalid data"),
-            @ApiResponse(responseCode = "404", description = "The user with the given id was not found")
-    })
     @PostMapping("user/remove")
     public UserCommonResponse userRemove(@RequestParam(name = "id") String userUUID) throws InvalidUserDateException, UserNotFoundException {
         return userRemoveUsecase.execute(userUUID);
     }
 
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "All users have been displayed")})
-    @Operation(summary = "Search all users", tags = {"User controller"})
-    @GetMapping("user/search")
     public UserSearchResponse showAllUsers() {
         return userShowAllUsecase.execute();
     }
